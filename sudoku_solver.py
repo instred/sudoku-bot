deb866deb15adc3be855e5e5609a5ecd5d49e425
instred <jakub.bok23@gmail.com>
2024-05-08 21:57:49 +0200

    code cleared, added comments

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -9,7 +9,9 @@ user_mode = False
 class Solver:
 
     def __init__(self, read_file_path: str='', save_file_path: str='') -> None:
-        
+
+
+        # Initializing the variables used for 9x9 board
         self.read_file_path = read_file_path
         self.save_file_path = save_file_path
         self.board_rows = 9
@@ -18,6 +20,7 @@ class Solver:
         self.read_table()
         
 
+    # Reading a board from txt file, each line is a row, 0s are empty spaces
     def read_table(self) -> None:
 
         try:
@@ -28,6 +31,8 @@ class Solver:
         except (FileNotFoundError, PermissionError, OSError):
             print("Error opening file")
 
+
+    # Helper function to save the answer to txt file
     def save_answer(self) -> None:
 
         with open(self.save_file_path, 'w') as f:
@@ -36,7 +41,7 @@ class Solver:
                 f.write('\n')
         
 
-
+    # Helper function for 'nicer' printing of the table
     def print_table(self) -> None:
         print()
         for i in range(self.board_rows):
@@ -50,6 +55,7 @@ class Solver:
             if (i+1) % 3 == 0:
                 print('\n')
 
+    # Function that finds the next empty space on board
     def find_next_empty(self) -> None | Tuple[int, int]:
         
         for row in range(self.board_rows):
@@ -59,31 +65,36 @@ class Solver:
                 
         return None
     
+
+    # Function to check whether the number we'd like to put in the board is correct
     def is_valid_number(self, position: Tuple[int, int], number: int) -> bool:
 
         square_size = int(sqrt(self.board_rows))
 
         row_idx, col_idx = position
 
+        # We check for the same number in row
         if number in self.sudoku_table[row_idx]:
             return False
         
         col_numbers = [row[col_idx] for row in self.sudoku_table]
 
+        # We check for the same number in column
         if number in col_numbers:
             return False
         
+        # Then we calculate the square position we are currently in
         square_x_idx = row_idx // square_size
         square_y_idx = col_idx // square_size
 
-        # print(square_x_idx, square_y_idx)
 
         square_nums = []
+        # Adding numbers in the square to the square list
         for row in range(square_x_idx*square_size, square_x_idx*square_size +square_size):
             for col in range(square_y_idx*square_size, square_y_idx*square_size +square_size):
                 square_nums.append(self.sudoku_table[row][col])
 
-        # print(square_nums)
+        # Check for the same number in the square
         if number in square_nums:
             return False
         
@@ -93,45 +104,30 @@ class Solver:
 
     def solve(self) -> bool:
 
+        # Finding next empty spot position
         next_empty = self.find_next_empty()
 
+        # If the function returns None, then the board is full and we can stop
         if not next_empty:
             return True
         
         row, col = next_empty
 
+        # Try to insert every number in current spot
         for i in range(1,10):
 
+            # If it's a valid place, then insert into the board
             if self.is_valid_number(position=(row, col), number=i):
                 self.sudoku_table[row][col] = i
 
+                # Continue solving board with recurring calls of solve
                 if self.solve():
                     return True
                 
+        # If we're here it means that we should backtrack, so reset the current cell and return False so we can continue previous call 
         self.sudoku_table[row][col] = 0
         return False
                 
-
-        
-
-    def check_valid(self) -> bool:
-
-        for row in range(self.board_rows):
-            for col in range(self.board_cols):
-                
-                if not self.is_valid_number(position=(row, col), number=self.sudoku_table[row][col]):
-                    print(row,col, self.sudoku_table[row][col])
-                    return False
-        
-        return True
-    
-    
-
-
-            
-        
-
-
 
 
 if __name__ == '__main__':
